6cf6c489a5a5448865f85f089028a8aa36297687
Stephen Farmer <safarmer@icloud.com>
2023-02-03 01:35:32 +0000

    Adding additional solution to same file

--- Main.scala
+++ Main.scala
@@ -25,3 +25,57 @@ object Main {
     }
   }
 }
+
+
+// import Main.Solution.lengthOfLongestSubstring
+
+// object Main {
+//   val test1: String = "abcabcbb"
+//   val test2: String = "bbbbb"
+//   val test3: String = "pwwkew"
+
+//   def main(args: Array[String]): Unit = {
+//     println("Hello world!")
+//     println(lengthOfLongestSubstring(test1))
+//     println(lengthOfLongestSubstring(test2))
+//     println(lengthOfLongestSubstring(test3))
+
+//   }
+
+//   object Solution {
+//     def lengthOfLongestSubstring(s: String): Int = {
+//       var subCount: Int = 0;
+//       var start: Int = 0
+//       var end: Int = 1
+//       var mem: String = ""
+
+//       println(mem)
+//       while(end != s.length - 1) {
+//         println("...")
+//         while(s.charAt(start) != s.charAt(end)) {
+//           mem = s.
+//           println(s.charAt(start) + " -- " + s.charAt(end))
+//           println("Substring:: " + s.substring(start, end + 1))
+//           subCount = s.substring(start, end + 1).length
+//           println("Substring length:: " + subCount)
+//           println("Start = " + start + " " + "End = " + end)
+//           end += 1
+//         }
+//         start += 1
+//       }
+//       if (s.isEmpty) 0
+//       else if (s.length == 1) 1
+//       else subCount
+//     }
+//   }
+// }
+
+// ignore file
+// count the values
+// constraints match?
+// add test cases
+// add other examples
+// push to GitHub
+// Time Complexity?
+// feedback from previous scala exercise (mettle?)
+
